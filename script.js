ffac959545edd15a1fdcf7cd85eab6a21afa6dcb
Valentijn.vw <valentijnvanwinden@gmail.com>
2021-09-08 11:17:28 +0000

    Begincode (nog niet getest)

--- script.js
+++ script.js
@@ -3,12 +3,34 @@
  * de code in deze functie wordt één keer uitgevoerd door
  * de p5 library, zodra het spel geladen is in de browser
  */
+
+const veldBreedte = 1280;
+const veldHoogte = 720;
+const aantalBalletjes = 5;
+var balletjes = [];
+
+function randomNummer(o,b) {
+  return Math.floor(Math.random() * (b - o + 1) + o);
+}
+
+function balletjeToevoegen() {
+  var x = randomNummer(0,veldBreedte);
+  var y = randomNummer(0,veldHoogte);
+  var snelheidX = randomNummer(1,4);
+  var snelheidY = randomNummer(1,4);
+  balletjes.push([x,y,snelheidX,snelheidY]);
+}
+
 function setup() {
   // Maak een canvas (rechthoek) waarin je je speelveld kunt tekenen
-  createCanvas(1280, 720);
+  createCanvas(veldBreedte, veldHoogte);
 
   // Kleur de achtergrond blauw, zodat je het kunt zien
   background('blue');
+
+  for(var i = 0;i < aantalBalletjes; i++) {
+    balletjeToevoegen();
+  }
 }
 
 
@@ -23,4 +45,10 @@ function draw() {
 
   // teken een cirkel
   ellipse(50,50,80,80);
+
+  balletjes.forEach(function(arr) {
+    ellipse(arr[0],arr[1],80,80);
+    arr[0] += arr[2];
+    arr[1] += arr[3];
+  });
 }
